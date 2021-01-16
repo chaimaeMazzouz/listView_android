b985f4d52f60d0f6246307eb45ba4792802f8b49
chaimaeMazzouz <mazzouz.chaimae.dev@gmail.com>
2021-01-16 20:59:32 +0100

    still work on

---
+++ app/src/main/java/com/example/listview_tp/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.listview_tp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity {
+
+    ListView list;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        list=findViewById(R.id.li1);
+        list.setAdapter(new versionAdapter(this));
+    }
+}
---
+++ app/src/main/java/com/example/listview_tp/versionAdapter.java
@@ -0,0 +1,49 @@
+package com.example.listview_tp;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class versionAdapter extends BaseAdapter {
+    private LayoutInflater layoutInflater;
+    private String title[] = { "Lollipop", "Marshmallow", "Nougat", "Oreo", "Pie", "Q", "R"};
+    private String sousTitle[] = { "version: 5.0", "version: 6.0", "version: 7.0", "version: 8.0", "version: 9.0", "version: 10.0", "version: 11.0"};
+    private int thumb[] = { R.drawable.lollipop, R.drawable.marshmallow, R.drawable.nougat, R.drawable.oreo, R.drawable.pie, R.drawable.q, R.drawable.r };
+    public versionAdapter(MainActivity main){
+        layoutInflater =(LayoutInflater)main.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+    }
+    @Override
+    public int getCount() {
+        return title.length;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return position;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View listItem = convertView;
+        int pos = position;
+        if (listItem == null)
+          listItem = layoutInflater.inflate(R.layout.list_item_layout, null);
+        ImageView imageView = listItem.findViewById(R.id.icon);
+        TextView titre =  listItem.findViewById(R.id.title);
+        TextView sousTitre =  listItem.findViewById(R.id.ss_titre);
+        imageView.setBackgroundResource(thumb[pos]);
+        titre.setText(title[pos]);
+        sousTitre.setText(sousTitle[pos]);
+        return listItem;
+    }
+}
